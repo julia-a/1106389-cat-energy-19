30af6ab86d533d6fe3398e2d549a5f6a1e574176
Julia Apreleva <12630m@mail.ru>
2020-05-11 19:54:03 +0300

    Доработка страниц под Pixel Perfect

--- gulpfile.js
+++ gulpfile.js
@@ -27,7 +27,6 @@ gulp.task("copy", function () {
   return gulp.src([
       "source/fonts/**/*.{woff,woff2}",
       "source/img/**",
-      "source/js/**",
       "source/*.ico"
     ], {
       base: "source"
